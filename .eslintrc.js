5344cb6064a12d8d9fde6f56105920cf6a6b6e8b
Yılmaz <yilmazcabuk@protonmail.com>
2024-04-12 22:07:23 +0300

    chore: update eslint configuration and package.json
    - Update ecmaVersion to 'latest' in .eslintrc.js
    - Add plugins 'simple-import-sort' and 'import' to .eslintrc.js
    - Add ESLint plugin "@eslint/js": "^9.0.0" to devDependencies in package.json
    - Update eslint version to "^8.57.0" in devDependencies of package.json
    - Add eslint-plugin-import "^2.29.1" and eslint-plugin-simple-import-sort "^12.0.0" to devDependencies in package.json
    - Add rules for simple-import-sort and import in .eslintrc.js
    - Add moduleNameMapper for path aliases in jest-e2e.json
    - Update module and moduleResolution to "NodeNext" in tsconfig.json
    - Add paths mapping for path aliases in tsconfig.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaVersion: 'latest',
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -15,11 +16,15 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
   },
 };
